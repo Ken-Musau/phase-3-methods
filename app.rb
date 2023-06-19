0eea8f72d83d692580d758c1c01fdf18bd9006ce
Kennedy Musau <kennedy.musauu@gmail.com>
2023-06-20 00:17:41 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,48 @@
+# def my_method param
+#     puts "Running myFunction"
+#     param + 1
+# end
+
+#  my_method(10)
+
+# def say_hello
+#     puts "Hello"
+# end
+
+# say_hello
+
+# def say_hi name = "Kennedy"
+#     puts "My name is #{name}"
+# end
+
+# # say_hi('Marge')
+
+# def add_and_log(num1, num2)
+#      puts num1 + num2
+#     #  num1 + num2
+# end
+
+# sum1 = add_and_log(2, 2)
+
+# puts sum1
+
+
+
+# def return_name
+#     "Kennedy Musau"
+# end
+
+# puts return_name
+
+
+
+def stylish_painter
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    puts best_hairstyle
+end
+
+puts stylish_painter
+
+
+
--- methods.rb
+++ methods.rb
@@ -1,1 +1,41 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet("Kennedy")
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add num1, num2
+    num1 + num2
+end
+
+puts add(1,2)
+
+# def halve num
+#     if num.class != Integer 
+#         return nil
+#     end
+
+#     num/2
+# end
+
+def halve num
+    return nil unless num.class == Integer
+    num/2
+end
+
+
+puts halve(10)
